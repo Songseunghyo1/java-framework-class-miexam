d099822e65e1b11cf87da3e1ca00792f8a1504b0
Songseunghyo1 <tmdgy15@gmail.com>
2018-04-20 16:43:08 +0900

    (참고)try~catch

--- src/main/java/kr/ac/jejunu/JejuConnectionMaker.java
+++ src/main/java/kr/ac/jejunu/JejuConnectionMaker.java
@@ -1,14 +1,25 @@
 package kr.ac.jejunu;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JejuConnectionMaker implements ConnectionMaker {
+    @Value("{db.classname}")
+    private String className = "com.mysql.jdbc.Driver";
+    @Value("{db.url}")
+    private String url = "jdbc:mysql://localhost/portalservice?characterEncoding=utf-8";
+    @Value("{db.username}")
+    private String username = "root";
+    @Value("{db.password}")
+    private String password = "sslabflask";
+
     @Override
     public Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName(className);
         return DriverManager.getConnection(
-                "jdbc:mysql://localhost/portalservice?characterEncoding=utf-8", "root", "sslabflask");
+                url, username, password);
     }
 }
